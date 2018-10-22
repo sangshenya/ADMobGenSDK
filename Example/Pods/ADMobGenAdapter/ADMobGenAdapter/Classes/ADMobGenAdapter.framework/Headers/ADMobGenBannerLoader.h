df10280bd0c849e09c06633ef9e955bbe3d673cb
陈坤 <sangshen@ecook.cn>
2018-10-22 16:09:40 +0800

    fix

---
+++ Example/Pods/ADMobGenAdapter/ADMobGenAdapter/Classes/ADMobGenAdapter.framework/Headers/ADMobGenBannerLoader.h
@@ -0,0 +1,18 @@
+//
+//  ADMobGenBannerLoader.h
+//  ADMobGenAdapter
+//
+//  Created by 曹飞 on 2018/7/13.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADMobGenBannerLoader : NSObject
+
++ (NSArray<NSString *> *)allPlatforms;
+
++ (void)registPlatformClass:(Class)klass forSdkName:(NSString *)sdkName;
+
++ (Class)platformClassForSdkName:(NSString *)sdkName;
+
+@end

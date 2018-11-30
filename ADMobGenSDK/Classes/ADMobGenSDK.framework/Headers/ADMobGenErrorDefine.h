92059449453c84d02ec4f7fa40efc3ea5d39f589
陈坤 <sangshen@ecook.cn>
2018-11-30 13:46:55 +0800

    代码梳理，删除旧代码

--- ADMobGenSDK/Classes/ADMobGenSDK.framework/Headers/ADMobGenErrorDefine.h
+++ ADMobGenSDK/Classes/ADMobGenSDK.framework/Headers/ADMobGenErrorDefine.h
@@ -14,6 +14,11 @@ typedef enum : NSInteger {
     ADMG_SDK_APPID_NIL = 1002, //appId为空
     ADMG_SDK_PACKAGE_ERROR = 1003,//包名和appId不对应
     ADMG_SDK_CONFIG_EMPTY = 1004,//请求配置为空
+    ADMG_SDK_LOADTIMEOUT  = -9995,//请求超时
+    ADMG_SDK_LOAD_FAIL = -9996,//获取广告失败
+    ADMG_SDK_PRATFORM_EMPTY = -9997,//未获取到加载广告的平台，可能一下原因 1、第一次启动未获取到配置信息；2、运营未配置广告
+    ADMG_SDK_LANDSCAPE_NOTLOADSPLASH = -9999,//横屏不加载开屏广告
+    
 } ADMobGenErrorCode;
 
 static NSError * ADMobGenError(NSErrorDomain domain, NSInteger code, NSString *localizedDescription) {

267e3fc2ddd9a75c8f44d1625f10c9a604cf3aee
陶冶明 <taoyeming@live.com>
2020-03-19 00:58:23 +0800

    暂时去除pods

--- Example/Pods/Target Support Files/Pods-ADMobGenSDK_Example/Pods-ADMobGenSDK_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ADMobGenSDK_Example : NSObject
-@end
-@implementation PodsDummy_Pods_ADMobGenSDK_Example
-@end
--- Example/Pods/Target Support Files/Pods-ADMobGenSDK_Tests/Pods-ADMobGenSDK_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ADMobGenSDK_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ADMobGenSDK_Tests
-@end
